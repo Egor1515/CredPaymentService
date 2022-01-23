28846be77b17aab5e326b778b62c5991ffc71191
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-01-23 16:39:28 +1000

    Initial commit

---
+++ src/CredPaymentService.java
@@ -0,0 +1,15 @@
+public class CredPaymentService {
+    public double calculate(int amount, int credterm, double irate) {
+
+
+        double x = (irate / credterm) / 100;
+        double z = x + 1;
+        double i = Math.pow(z, credterm);
+        double payment = (x * i) / (i - 1) * (amount);
+
+        return payment;
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        CredPaymentService service = new CredPaymentService();
+
+        double payment1 = service.calculate(1_000_000, 12, 9.99);
+        System.out.println("Ваш платеж по кредиту составляет " + payment1);
+        double payment2 = service.calculate(1_000_000, 24, 9.99);
+        System.out.println("Ваш платеж по кредиту составляет " + payment2);
+        double payment3 = service.calculate(1_000_000, 36, 9.99);
+        System.out.println("Ваш платеж по кредиту составляет " + payment3);
+
+
+    }
+}
